e1ed984e2a9b31bfae013bcf08a7d626f1a16ff6
Goncharov Anton <goncharrov.a@icloud.com>
2025-12-03 10:08:13 +0300

    test: add pure selene test without allure setup

---
+++ test_pure_selene.py
@@ -0,0 +1,9 @@
+from selene import browser, have
+
+
+def test_issue_title_is_visible():
+    repo = "goncharrov-a/lesson_10"
+    expected_title = "Example_issue"
+    browser.open(f"https://github.com/{repo}/issues")
+    browser.all('[data-testid="issue-pr-title-link"]').element_by(have.exact_text(expected_title)).click()
+    browser.element('[data-testid="issue-title"]').should(have.exact_text(expected_title))
