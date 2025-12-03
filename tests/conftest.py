748aa751829f6be95703418250d93b4db8cf9b01
Goncharov Anton <goncharrov.a@icloud.com>
2025-12-03 11:15:27 +0300

    docs: upd browser setup settings

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_browser():
+    browser.config.driver_name = 'chrome'
+    browser.config.window_width = 1440
+    browser.config.window_height = 1080
+    browser.config.save_screenshot_on_failure = True
+    browser.config.reports_folder = 'reports'
+
+    yield
+    browser.quit()
