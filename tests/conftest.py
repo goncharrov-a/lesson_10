2eff2e64ce1cd3e5e17e51d1c74172fea2558e94
Goncharov Anton <goncharrov.a@icloud.com>
2025-12-03 13:03:13 +0300

    upd settings for gh actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,26 @@
+# conftest.py
+
 import pytest
-from selene import browser
+from selene import browser, Config
+from selenium.webdriver import ChromeOptions
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(autouse=True)
 def setup_browser():
-    browser.config.driver_name = 'chrome'
-    browser.config.window_width = 1440
-    browser.config.window_height = 1080
-    browser.config.save_screenshot_on_failure = True
-    browser.config.reports_folder = 'reports'
+    options = ChromeOptions()
+    options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    browser.config = Config(
+        driver_options=options,
+        timeout=6.0,
+        window_width=1920,
+        window_height=1080,
+    )
 
     yield
+
     browser.quit()
