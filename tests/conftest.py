581272b73cd11bb9113d305870962eea78aa6ab8
Goncharov Anton <goncharrov.a@icloud.com>
2025-12-03 13:21:31 +0300

    upd settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,22 @@
-# conftest.py
-
 import pytest
-from selene import browser, Config
-from selenium.webdriver import ChromeOptions
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(autouse=True)
-def setup_browser():
-    options = ChromeOptions()
-    options.add_argument("--headless=new")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
+@pytest.fixture(scope="function")
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
 
-    browser.config = Config(
-        driver_options=options,
-        timeout=6.0,
-        window_width=1920,
-        window_height=1080,
-    )
+    driver = webdriver.Chrome(options=chrome_options)
 
-    yield
+    browser.config.driver = driver
+    browser.config.base_url = "https://github.com"
+    browser.config.timeout = 6
 
-    browser.quit()
+    yield driver
+    driver.quit()
